06d61db871725e9d11702225925ac7b792b73879
programmer-shaheb <abidurrahman471@gmail.com>
2023-05-29 12:14:45 +0600

    [DONE} setup express, dotenv, cors, mongoose, dbConnection, parser

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.join(process.cwd(), ".env"),
+});
+
+export default {
+  port: process.env.PORT,
+  dbUri: process.env.DB_URI,
+};
